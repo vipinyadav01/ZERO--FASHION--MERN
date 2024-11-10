fec8e8ef6ac3e24b8304e6820e8ff77524ff9857
vipin yadav <vipinyadav9m@gmail.com>
2024-11-10 20:22:40 +0530

    main

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify"; 
-import "react-toastify/dist/ReactToastify.css"; 
 
-// Configure toast settings globally
-toast.configure({
-  position: toast.POSITION.BOTTOM_CENTER,
-  autoClose: 2000, // 2 seconds
-  hideProgressBar: true,
-  closeOnClick: true,
-  pauseOnHover: false,
-  draggable: false,
-  progress: undefined,
-});
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
-    <ToastContainer /> 
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
 );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,25 +3,11 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-// Configure toast settings globally
-toast.configure({
-  position: toast.POSITION.BOTTOM_CENTER,
-  autoClose: 2000, // 2 seconds
-  hideProgressBar: true,
-  closeOnClick: true,
-  pauseOnHover: false,
-  draggable: false,
-  progress: undefined,
-});
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ShopContextProvider>
       <App />
-      <ToastContainer />
     </ShopContextProvider>
   </BrowserRouter>
 );
