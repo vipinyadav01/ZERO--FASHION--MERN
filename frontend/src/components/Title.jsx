576e4a6560176b1ab69ae0b55ebf3b90b71abb54
vipinyadav01 <vipinyadav9m@gmail.com>
2025-08-28 22:39:10 +0530

    style: Update Title component for improved styling and layout
    - Adjusted spacing and font sizes in the Title component for better visual hierarchy and consistency.
    - Enhanced the separator line's width and height to align with the updated design standards.

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Title({ text1, text2 }) {
   return (
-    <div className="inline-flex gap-2 items-center mb-3 text-xl">
+    <div className="inline-flex gap-3 items-center mb-4 text-2xl sm:text-2xl lg:text-3xl">
       <span className="text-gray-500 font-['Asterion']">
         {text1}
       </span>
       <span className="text-gray-700 font-medium font-['Asterion']">
         {text2}
       </span>
-      <span className="w-8 sm:w-12 h-0.5 bg-gray-700 block"></span>
+      <span className="w-12 sm:w-16 lg:w-20 h-1 bg-gray-700 block"></span>
     </div>
   )
 }
